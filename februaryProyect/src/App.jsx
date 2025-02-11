2164b4ad22b6776f931dedaa17f5f08c66618a0e
Alanav29 <josephalannavarro@gmail.com>
2025-02-11 00:40:29 -0600

    Primero pasos reproductor

---
+++ februaryProyect/src/App.jsx
@@ -0,0 +1,105 @@
+import "./App.css";
+import ButtonsBox from "./generalComponents/ButtonsBox";
+
+function App() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center m-5">
+      <div className="cassette">
+        <div className="top-tape">
+          <div className="arrow-mark-container">
+            <div className="arrow-mark">🡁</div>
+          </div>
+          <div className="brand-mark">SONY</div>
+        </div>
+        <div className="medium-tape">
+          <div className="label-container">
+            <div className="label">
+              <div className="top-label">
+                <div className="predefined-text">INDEX</div>
+                <div className="top-text">
+                  <div>Love</div>
+                  <div>Memories (First Time Version)</div>
+                </div>
+              </div>
+              <div className="medium-label">
+                <div className="side-name">A</div>
+                <div className="gap-container">
+                  <div className="gap">
+                    <div className="left tapereel">
+                      <div className="notch notch-1"></div>
+                      <div className="notch notch-2"></div>
+                      <div className="notch notch-3"></div>
+                    </div>
+                    <div className="central-gap">
+                      <div className="left innertape"></div>
+                      <div className="right innertape"></div>
+                    </div>
+                    <div className="right tapereel">
+                      <div className="notch notch-1"></div>
+                      <div className="notch notch-2"></div>
+                      <div className="notch notch-3"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="noise-reduction">
+                  NOISE REDUCTION
+                  <div className="box">
+                    <div>ON</div>
+                    <div>OFF</div>
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-label">
+                <div className="brand-text">
+                  <div className="maintext">SONY</div>
+                  <div className="subtext">TYPE I (NORMAL) POSITION</div>
+                </div>
+                <div className="arrow-text">
+                  <span>🡆</span>
+                </div>
+                <div className="model-text">
+                  <span>AHF</span>46
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bottom-tape">
+          <div className="magnetic-shield-container">
+            <div className="magnetic-shield">
+              <div className="left-hole-container">
+                <div className="hole hole-1"></div>
+                <div className="hole hole-2"></div>
+              </div>
+              <div className="screw-container">
+                <div className="screw"></div>
+              </div>
+              <div className="right-hole-container">
+                <div className="hole hole-3"></div>
+                <div className="hole hole-4"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="screw screw-1">
+          <div className="dot"></div>
+        </div>
+        <div className="screw screw-2">
+          <div className="dot"></div>
+        </div>
+        <div className="screw screw-3">
+          <div className="dot"></div>
+        </div>
+        <div className="screw screw-4">
+          <div className="dot"></div>
+        </div>
+      </div>
+
+      <div className="controls-container">
+        <ButtonsBox />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ februaryProyect/src/generalComponents/ButtonsBox.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import "../styles/ButtonsBox.css";
+
+const ButtonsBox = () => {
+  const [songs, setSongs] = useState(["dust", "city"]);
+  const [currentSong, setCurrentSong] = useState(0);
+  const audioButton = new Audio("/sounds/click.mp3"); // Ruta al archivo de sonido
+  const playSound = () => {
+    audioButton.play(); // Reproduce el sonido
+  };
+
+  const [audioSong, setAudioSong] = useState(new Audio(`/sounds/city.mp3`));
+  // Ruta al archivo de sonido
+  const playFirstSong = () => {
+    playSound();
+    audioSong.play(); // Pausa el sonido
+  };
+
+  const pauseSong = () => {
+    playSound();
+    audioSong.pause(); // Pausa el sonido
+  };
+
+  const plusSong = () => {
+    console.log(currentSong + 1);
+
+    setCurrentSong(currentSong + 1);
+    audioSong.pause();
+    audioSong.src = `/sounds/${songs[currentSong]}.mp3`; // Ruta al archivo de sonido
+    audioSong.play(); // Reproduce el sonido
+  };
+
+  const minusSong = () => {
+    setCurrentSong(currentSong - 1);
+    audioSong.pause();
+    audioSong.src = `/sounds/${songs[currentSong]}.mp3`; // Ruta al archivo de sonido
+    audioSong.play(); // Reproduce el sonido
+  };
+  return (
+    <div>
+      <div className="main mt-5">
+        <div className="buttons d-flex flex-row">
+          <div className="button_pair me-3">
+            <div className="btn-player">
+              <button
+                className="button3 d-flex align-items-center"
+                onClick={playFirstSong}
+              >
+                <i className="bi bi-play-fill fs-1 text-white"></i>
+              </button>
+            </div>
+            <div className="btn-player">
+              <button
+                className="button4 d-flex align-items-center"
+                onClick={pauseSong}
+              >
+                <i className="bi bi-pause-fill fs-1 text-white"></i>
+              </button>
+            </div>
+          </div>
+          <div className="button_pair ms-3">
+            <div className="btn-player">
+              <button className="button1" onClick={minusSong}>
+                <svg
+                  viewBox="0 -960 960 960"
+                  className="svg1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M200-440v-80h560v80H200Z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="btn-player">
+              <button className="button2" onClick={plusSong}>
+                <svg
+                  viewBox="0 -960 960 960"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="svg2"
+                >
+                  <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonsBox;
